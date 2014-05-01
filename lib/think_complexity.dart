650b75903756f9b530933d5f18b6868af9c47d35
Enrique Gavidia <enrique@enriquegavidia.com>
2014-05-01 15:02:14 -0700

    added better testing

--- lib/think_complexity.dart
+++ lib/think_complexity.dart
@@ -5,37 +5,61 @@ part 'edge.dart';
 part 'graph.dart';
 part 'random_graph.dart';
 
+List<int>
+valid_regular_degrees(int graph_vertices) {
+    assert(graph_vertices > 1);
+
+    List valid_degrees = [];
+    int degree_increment;
+
+    if (graph_vertices.isEven)
+        degree_increment = 1;
+    else
+        degree_increment = 2;
+
+    for (int i = degree_increment; i < graph_vertices; i += degree_increment) {
+        valid_degrees.add(i);
+    }
+
+    return valid_degrees;
+}
+
+
+const int
+GRAPH_VERTEX_COUNT = 6;
+
+const List vertex_names = const ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 main() {
-    var vertices = [
-        new Vertex('a'),
-        new Vertex('b'),
-        new Vertex('c'),
-        new Vertex('d'),
-        new Vertex('e'),
-        new Vertex('f'),
-        new Vertex('g'),
-        new Vertex('h'),
-        new Vertex('i'),
-        new Vertex('j')
-    ];
+    List<Vertex> vertices = new List.generate(GRAPH_VERTEX_COUNT, (index) => new Vertex(vertex_names[index]) );
 
-    var vertex_count = 6;
-    var regular_degree = 4;
+    var g = new Graph(vertices);
+    print("Edge-less Graph with ${g.vertices.length} vertices\n");
 
-    var g = new Graph(vertices.take(vertex_count));
-    print("Init Edge-less Graph: ${g.vertices.length} vertices");
+    for (int degrees in valid_regular_degrees(g.vertices.length)) {
+        print('Adding Regular Edges with $degrees Degrees...');
+        g.add_regular_edges(degrees);
 
-    g.add_regular_edges(regular_degree);
+        print("  Is Regular: ${g.is_regular}");
 
-    var first_degree = g.degree_of(vertices[0]);
-    print("Degrees at '${vertices[0].label}': $first_degree");
+        var first_degree = g.degree_of(vertices[0]);
+        bool regular_with_degrees = g.is_regular && first_degree == degrees;
 
-    print("Is Regular: ${g.is_regular}");
+        print("  Is Regular with $degrees degrees: $regular_with_degrees");
 
-    g.edges.forEach((edge) => print("${edge.v1.label} - ${edge.v2.label}"));
+        if (!regular_with_degrees) {
+            print("  Degrees at '${vertices[0].label}': $first_degree");
+        }
 
-    print("Is Regular with $regular_degree degrees: ${g.is_regular && first_degree == regular_degree}");
+        if (!g.is_regular) {
+            print("  Edges:");
+            g.edges.forEach((edge) => print("    ${edge.v1.label} - ${edge.v2.label}"));
+
+            print("  Vertex Degrees:");
+            g.vertices.forEach((vertex) => print("    ${vertex.label}: ${g.degree_of(vertex)}"));
+        }
+        print('');
+    }
 
     //g.add_all_edges();
     //print("Is Complete: ${g.is_complete}");
